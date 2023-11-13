d828108b56bc44c4fbc5e286fab80864eeeb2616
Casper Schuijt <casperschuijt@gmail.com>
2023-11-13 23:52:45 +0100

    One additional test

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -8,25 +8,42 @@
 #include "Ice.hpp"
 
 int main( void ) {
-	IMateriaSource* src = new MateriaSource();
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
+	{
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
 
-	ICharacter* me = new Character("me");
+		ICharacter* me = new Character("me");
 
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
 
-	ICharacter* bob = new Character("bob");
-	me->use(0, *bob);
-	me->use(1, *bob);
+		ICharacter* bob = new Character("bob");
+		me->use(0, *bob);
+		me->use(1, *bob);
 
-	delete bob;
-	delete me;
-	delete src;
+		delete bob;
+		delete me;
+		delete src;
+	}
+
+	{
+		IMateriaSource* src  = new MateriaSource();
+		IMateriaSource* src2 = new MateriaSource();
+
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+
+		src2->learnMateria(new Ice());
+
+		*src = *src2;
+
+		delete src;
+		delete src2;
+	}
 
 	return 0;
 }
